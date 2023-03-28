be246b048a9bfe119e710deb1387c44dbe5401a8
EOGAMES <113043641+EOGames@users.noreply.github.com>
2023-03-28 14:58:35 +0530

    CRUD made  with EJS

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/',(req,res)=>
     res.render('pages/index');
 });
 
-app.post('/',(req,res)=>
+app.post('/save',(req,res)=>
 {
   //  console.log(req.body);
   HandleStorage(req.body);
@@ -50,8 +50,53 @@ app.get('/data',(req,res)=>
     }
     else
     {
-        res.send('Please Add Something');        
+        res.send('');        
     }
+});
+
+app.get('/edit',(req,res)=>
+{
+    res.render('pages/edit')
+});
+
+app.put('/edit/:id',(req,res)=>
+{
+    console.log(req.params.id);
+    console.log(req.body);
+
+    data = JSON.parse(localstorage.getItem('data'));
+    if (data.length >0)
+    {
+        data[req.params.id] = req.body;
+
+        localstorage.setItem('data',JSON.stringify(data));
+    }
+    res.send('updated');
+});
+
+app.delete('/delselected/:id',(req,res)=>
+{
+    console.log(req.params.id);
+    if (localstorage.getItem('data')!== null)
+    {
+        let data = [];
+        data = JSON.parse(localstorage.getItem('data'));
+        
+        data.splice(req.params.id,1);
+
+        localstorage.setItem('data',JSON.stringify(data));
+        console.log('deleted');
+    }
+    res.send('deleted Selected');
+});
+
+app.delete('/deleteall',(req,res)=>
+{
+    if (localstorage.getItem('data')!==null)
+    {
+        localstorage.removeItem('data');
+    }
+    res.send('done');
 });
 
 app.listen(5700,()=>
@@ -68,7 +113,7 @@ function HandleStorage(d)
     {
         //means data exist
         data = JSON.parse(localstorage.getItem('data'));
-        console.log("Data Found");
+       // console.log("Data Found");
 
         if (data.length <=0)
         {
@@ -86,5 +131,5 @@ function HandleStorage(d)
     console.log('count is '+count);
     data[count] = d;
     localstorage.setItem('data',JSON.stringify(data));
-    console.log(data);
+    //console.log(data);
 }
